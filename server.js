76e0ea69c55dd9cd5ccb5bb24408ea4a25db7cfc
Memphis83 <kiran.iyer83@gmail.com>
2021-05-04 19:55:42 +0530

    working getPost route and post.ejs, now able to access post page

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
-const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const postRoutes = require('./routes/posts')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
 
-require('dotenv').config({path: './config/.env'})
+require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
-connectDB()
+connectDB();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
 
 //----------JOHN'S CODE adds the following lines---
 //Use forms for put / delete
@@ -30,23 +30,23 @@ app.use(logger('dev'))
 
 // Sessions
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-app.use(flash())
-  
-app.use('/', mainRoutes)
-app.use('/post', postRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
