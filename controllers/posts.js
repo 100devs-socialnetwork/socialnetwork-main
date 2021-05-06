9b5209e0ee7c5e5682e0e061a50ae8b18a576efd
relspeedwagon <74011011+relspeedwagon@users.noreply.github.com>
2021-05-06 16:56:26 -0400

    few more ejs bug fixes, fixes css link issue for post.ejs! adds a little more css layout and button styling

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,7 @@ const Post = require("../models/Post");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
+      const posts = await Post.find({ user: req.user.id }).sort({ createdAt: "desc" }).lean();;
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
